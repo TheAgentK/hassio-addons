bfe9c2b20afdea4e329caf238fc18de923e2a092
Karsten Siedentopp <karsten.siedentopp@adesso.de>
2022-10-14 09:26:08 +0200

    change git-repo-url

--- googlewifi-dashboard/run.sh
+++ googlewifi-dashboard/run.sh
@@ -3,5 +3,7 @@
 export REFRESH_TOKEN=$(bashio::config 'refresh_token')
 bashio::log.info "Refresh token configured as ${REFRESH_TOKEN}."
 
+ls -la
+
 bashio::log.info "Starting dashboard."
 npm run serve
