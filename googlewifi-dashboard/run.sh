bd131d8084d9fa1312b249f91ae18c9f35a06b09
Karsten Siedentopp <karsten.siedentopp@adesso.de>
2022-10-14 10:44:06 +0200

    change port

--- googlewifi-dashboard/run.sh
+++ googlewifi-dashboard/run.sh
@@ -3,11 +3,6 @@
 export REFRESH_TOKEN=$(bashio::config 'refresh_token')
 bashio::log.info "Refresh token configured as ${REFRESH_TOKEN}."
 
-ls -la
-
-pwd
-
-cd google-wifi-dashboard
-
 bashio::log.info "Starting dashboard."
+cd google-wifi-dashboard
 npm run serve
