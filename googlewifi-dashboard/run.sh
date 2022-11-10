badf96aeaaf3b02d2be8e18eb53928548bacdc1c
Karsten Siedentopp <karsten.siedentopp@adesso.de>
2022-11-10 22:29:56 +0100

    googlewifi-dashboard: fixed ingress errors

--- googlewifi-dashboard/run.sh
+++ googlewifi-dashboard/run.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/with-contenv bashio
 
-export REFRESH_TOKEN=$(bashio::config 'refresh_token')
-bashio::log.info "Refresh token configured as ${REFRESH_TOKEN}."
+export INGRESS_ENTRY=$(bashio::addon.ingress_entry)
+export VITE_HA_REFRESH_TOKEN=$(bashio::config 'refresh_token')
+
+bashio::log.info "Refresh token configured as ${VITE_HA_REFRESH_TOKEN}."
 
 bashio::log.info "Building dashboard."
-cd /tmp/dashboard
-npm run build
+cd /tmp/google-wifi-dashboard
+npm run build -- --base=${INGRESS_ENTRY}
+ln -s /tmp/google-wifi-dashboard/dist/ /var/www/html
 
 bashio::log.info "Start NGINX"
 nginx -g 'daemon off;error_log /dev/stdout debug;'
