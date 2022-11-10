4d5f947b6232cadb31fdf96c0393fcd8e8fc11b7
Karsten Siedentopp <karsten.siedentopp@adesso.de>
2022-11-10 17:43:50 +0100

    googlewifi-dashboard: removed unused code and use seperate repo for dashboard

--- googlewifi-dashboard/dashboard/src/views/NotFound.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h1>page not found</h1>
-        <button @click="$router.push('/')">go home</button>
-    </div>
-</template>
--- googlewifi-dashboard/dashboard/vue.config.js
+++
@@ -1,9 +0,0 @@
-try {
-  process.env.VUE_APP_HA_REFRESH_TOKEN =
-    require("/data/options.json").refresh_token;
-} catch (e) {}
-
-module.exports = {
-  publicPath: "",
-  outputDir: "/var/www/html",
-};
